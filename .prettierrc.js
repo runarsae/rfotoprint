892aa6165beef831baa4dca85f6436e395603b1c
Runar Sæther <runarsather@hotmail.com>
2021-01-09 15:00:39 +0100

    Install dependencies

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    semi: true,
+    trailingComma: 'none',
+    singleQuote: true,
+    printWidth: 80,
+    tabWidth: 4,
+    tabs: true,
+    bracketSpacing: true,
+    jsxBracketsSameLine: false,
+    arrowParents: 'always',
+    endOfLine: 'auto'
+};
