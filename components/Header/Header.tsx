4c288097ab9df474c23087dae92d798050899ea6
Runar Sæther <runarsather@hotmail.com>
2021-01-11 13:56:29 +0100

    Horisontal header on larger screens (#10)

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
+import Link from 'next/link';
 import Navigation from './Navigation';
 
 const Wrapper = styled.div`
@@ -13,12 +14,26 @@ const Wrapper = styled.div`
     }
 `;
 
+const Content = styled.div`
+    display: grid;
+    grid-template-columns: 1fr;
+    max-width: 1300px;
+    margin: auto;
+    justify-items: center;
+    align-items: center;
+    gap: 20px;
+
+    @media (min-width: 830px) {
+        justify-items: left;
+        grid-template-columns: 1fr auto;
+    }
+`;
+
 const Logo = styled.div`
     display: block;
     position: relative;
     width: 160px;
     height: 77.02px;
-    margin: auto;
     -webkit-user-select: none; /* Safari */
     -moz-user-select: none; /* Firefox */
     -ms-user-select: none; /* IE10+/Edge */
@@ -33,10 +48,20 @@ const Logo = styled.div`
 function Header(): JSX.Element {
     return (
         <Wrapper>
-            <Logo>
-                <Image src="/logo.png" alt="Rossland Fotoprint" layout="fill" />
-            </Logo>
-            <Navigation />
+            <Content>
+                <Logo>
+                    <Link href="/hjem">
+                        <a>
+                            <Image
+                                src="/logo.png"
+                                alt="Rossland Fotoprint"
+                                layout="fill"
+                            />
+                        </a>
+                    </Link>
+                </Logo>
+                <Navigation />
+            </Content>
         </Wrapper>
     );
 }
