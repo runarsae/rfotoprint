b006c13b56c6deb1d72fd20a294c32c3dd6e613d
Runar Sæther <runarsather@hotmail.com>
2021-08-14 12:09:19 +0200

    Add new contact icons (#26)

--- rfotoprint/src/components/common/Icons.tsx
+++ rfotoprint/src/components/common/Icons.tsx
@@ -1,14 +1,15 @@
 interface IconProps {
     fill: string;
+    size?: number;
 }
 
 export const DeleteIcon = (props: IconProps) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="24px"
             viewBox="0 0 24 24"
-            width="24px"
+            width={props.size ? props.size + 'px' : '24px'}
+            height={props.size ? props.size + 'px' : '24px'}
             fill={props.fill}
         >
             <path d="M0 0h24v24H0V0z" fill="none" />
@@ -21,9 +22,9 @@ export const EditIcon = (props: IconProps) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="24px"
             viewBox="0 0 24 24"
-            width="24px"
+            width={props.size ? props.size + 'px' : '24px'}
+            height={props.size ? props.size + 'px' : '24px'}
             fill={props.fill}
         >
             <path d="M0 0h24v24H0V0z" fill="none" />
@@ -36,13 +37,58 @@ export const AddIcon = (props: IconProps) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="24px"
             viewBox="0 0 24 24"
-            width="24px"
+            width={props.size ? props.size + 'px' : '24px'}
+            height={props.size ? props.size + 'px' : '24px'}
             fill={props.fill}
         >
             <path d="M0 0h24v24H0z" fill="none" />
             <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+        </svg>
+    );
+};
+
+export const PhoneIcon = (props: IconProps) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width={props.size ? props.size + 'px' : '24px'}
+            height={props.size ? props.size + 'px' : '24px'}
+            fill={props.fill}
+        >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
+        </svg>
+    );
+};
+
+export const MailIcon = (props: IconProps) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width={props.size ? props.size + 'px' : '24px'}
+            height={props.size ? props.size + 'px' : '24px'}
+            fill={props.fill}
+        >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
+        </svg>
+    );
+};
+
+export const LocationIcon = (props: IconProps) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width={props.size ? props.size + 'px' : '24px'}
+            height={props.size ? props.size + 'px' : '24px'}
+            fill={props.fill}
+        >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
         </svg>
     );
 };
