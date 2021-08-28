da65753de364f791a380f5aff1ec91f31c189039
Runar Sæther <runarsather@hotmail.com>
2021-08-28 21:16:41 +0200

    Change language on facebook page embed (#26)

--- rfotoprint/src/components/footer/External.tsx
+++ rfotoprint/src/components/footer/External.tsx
@@ -18,18 +18,10 @@ const Wrapper = styled.div`
 
 const FacebookWrapper = styled.div`
     display: block;
-    width: 280px;
+    width: 250px;
     height: 130px;
-
-    @media (min-width: 1080px) {
-        width: 250px;
-    }
-`;
-
-const Embed = styled.iframe`
-    display: block;
     border-radius: 2px;
-    border: none;
+    overflow: hidden;
 `;
 
 const Title = styled.h3`
@@ -88,25 +80,26 @@ function External(): JSX.Element {
         <Wrapper>
             <Title>Facebook</Title>
             <FacebookWrapper>
-                {screenWidth < 1080 ? (
-                    <Embed
-                        title="Facebook"
-                        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FR-Fotoprint-112650973637204&tabs&width=280&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
-                        width="280"
-                        height="130"
-                        scrolling="no"
-                        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-                    ></Embed>
-                ) : (
-                    <Embed
-                        title="Facebook"
-                        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FR-Fotoprint-112650973637204&tabs&width=250&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
-                        width="250"
-                        height="130"
-                        scrolling="no"
-                        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-                    ></Embed>
-                )}
+                <div
+                    className="fb-page"
+                    data-href="https://www.facebook.com/profile.php?id=100063615033077"
+                    data-tabs=""
+                    data-width="250"
+                    data-height="130"
+                    data-small-header="false"
+                    data-adapt-container-width="true"
+                    data-hide-cover="false"
+                    data-show-facepile="false"
+                >
+                    <blockquote
+                        cite="https://www.facebook.com/profile.php?id=100063615033077"
+                        className="fb-xfbml-parse-ignore"
+                    >
+                        <a href="https://www.facebook.com/profile.php?id=100063615033077">
+                            R Fotoprint
+                        </a>
+                    </blockquote>
+                </div>
             </FacebookWrapper>
             <Title>Media</Title>
             <Link
