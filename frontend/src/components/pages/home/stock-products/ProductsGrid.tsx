5e5610188ca969185d67c26ebc451ef4ec96c679
Runar Sæther <runarsather@hotmail.com>
2022-01-02 19:55:26 +0100

    Fix page size depending on window width (#67)

--- frontend/src/components/pages/home/stock-products/ProductsGrid.tsx
+++ frontend/src/components/pages/home/stock-products/ProductsGrid.tsx
@@ -2,12 +2,15 @@ import styled, { useTheme } from 'styled-components';
 import Product from './Product';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import {
+    pageSizeState,
     popupProductImageState,
     productsErrorState,
     productsState
 } from '../../../../state/products';
 import Typography from '../../../common/Typography';
 import { popupOpenState, popupTypeState } from '../../../../state/popup';
+import useWindowDimensions from '../../../../util/windowDimensions';
+import { useEffect } from 'react';
 
 const Grid = styled.div((props) => ({
     display: 'grid',
@@ -31,17 +34,30 @@ const Grid = styled.div((props) => ({
 function ProductsGrid() {
     const theme = useTheme();
 
+    const { width } = useWindowDimensions();
+
     const products = useRecoilValue(productsState);
     const productsError = useRecoilValue(productsErrorState);
+
+    const setPageSize = useSetRecoilState(pageSizeState);
 
     const setPopupProductImage = useSetRecoilState(popupProductImageState);
     const setPopupType = useSetRecoilState(popupTypeState);
     const setPopupOpen = useSetRecoilState(popupOpenState);
 
+    // Change page size based on number of grid cells according to window width
+    useEffect(() => {
+        if (width >= theme.breakpoints.lg && width < theme.breakpoints.xl) {
+            setPageSize(9);
+        } else {
+            setPageSize(8);
+        }
+    }, [width]);
+
     return (
         <>
             {productsError ? (
-                <Typography variant="body1" align="center" color={theme.palette.error}>
+                <Typography align="center" color={theme.palette.error}>
                     {productsError}
                 </Typography>
             ) : products && products.length > 0 ? (
