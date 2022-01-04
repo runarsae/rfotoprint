a2d7a58a97f7e0ac636db6a7cd777a48a82ce7fe
Runar Sæther <runarsather@hotmail.com>
2022-01-05 00:27:40 +0100

    Add logo (#67)

--- frontend/src/components/pages/not-found/index.tsx
+++ frontend/src/components/pages/not-found/index.tsx
@@ -1,15 +1,26 @@
+import styled from 'styled-components';
 import Section from '../../common/Section';
 import Typography from '../../common/Typography';
+import logo from '../../../logo/logo-2-dark.svg';
+
+const Logo = styled.img({
+    display: 'block',
+    margin: 'auto',
+    width: '200px'
+});
 
 function NotFound() {
     return (
         <Section>
-            <Typography variant="h1" align="center">
-                404
-            </Typography>
-            <Typography variant="body1" align="center">
-                Beklager, den forespurte siden ble ikke funnet.
-            </Typography>
+            <Logo src={logo} alt="Rossland Fotoprint" />
+            <div>
+                <Typography variant="h1" align="center" fontSize="32px" marginBottom>
+                    404
+                </Typography>
+                <Typography variant="body1" align="center">
+                    Beklager, den forespurte siden ble ikke funnet.
+                </Typography>
+            </div>
         </Section>
     );
 }
